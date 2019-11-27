89232c578f41bf621df44479d5ccb75d4d5999cf
Rodrigo Martin Martin <rodrigomartin.vg@gmail.com>
2019-11-27 06:53:31 +0000

    algo

---
+++ heapsort/heapsort.go
@@ -0,0 +1,5 @@
+package heapsort
+
+//import "fmt"
+
+var ASD string = "hi"
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+  "fmt"
+  "heapsort"
+
+)
+
+
+func main() {
+  fmt.Println(heapsort.ASD)
+
+}
